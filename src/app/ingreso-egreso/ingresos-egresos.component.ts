d02746292f54f4f5a06607305a40c6f5dad92883
Angel Miranda <ang.master@gmail.com>
2020-07-25 11:56:29 -0400

    Primer Commit

---
+++ src/app/ingreso-egreso/ingresos-egresos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresos-egresos',
+  templateUrl: './ingresos-egresos.component.html',
+  styles: [
+  ]
+})
+export class IngresoEgresoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
